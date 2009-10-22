985946191343766bbb0b67ad4f74b3524e651541
wabu <waeber@inf.fu-berlin.de>
2009-10-22 17:55:59 +0200

    added k parameter

--- u01/src/main/java/de/berlin/fu/inf/pattern/u02/Main.java
+++ u01/src/main/java/de/berlin/fu/inf/pattern/u02/Main.java
@@ -1,20 +1,16 @@
 package de.berlin.fu.inf.pattern.u02;
 
-import java.util.StringTokenizer;
-
 import de.berlin.fu.inf.pattern.u02.control.Controller;
 
 public class Main {
 	public static void main(String[] args) {
 		Controller controller = new Controller();
 		
-		StringTokenizer t = new StringTokenizer(" 1 2 3");
-		
-		System.out.println(t.nextToken());
-		
 		System.out.println("learning ...");
 		controller.learnFromFile("pendigits-training.txt");
-		System.out.println("testing ...");
-		System.out.println(controller.testOnFile("pendigits-testing.txt"));
+		for(int k=1; k<8; k++){
+			System.out.println("testing with k="+k+"...");
+			System.out.println(controller.testOnFile("pendigits-testing.txt", k));
+		}
 	}
 }
