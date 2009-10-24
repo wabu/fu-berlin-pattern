1b941017e09f9914572dcfe900c2a1dfb1bba436
wabu <waeber@inf.fu-berlin.de>
2009-10-24 16:38:44 +0200

    added functions to change dimension

--- pattern-lib/src/main/java/de/berlin/fu/inf/pattern/data/kdtree/DimensionComparator.java
+++ pattern-lib/src/main/java/de/berlin/fu/inf/pattern/data/kdtree/DimensionComparator.java
@@ -3,9 +3,16 @@ package de.berlin.fu.inf.pattern.data.kdtree;
 import java.util.Comparator;
 
 
+// XXX we could make this a immutable/functionalstyle object
 
 public class DimensionComparator<D extends Dimensionable<D>> implements Comparator<D> {
+	final private int maxDimension;
 	private int currentDimension;
+
+	public DimensionComparator(int maxDimension) {
+		this.maxDimension = maxDimension;
+		this.currentDimension = 0;
+	}
 
 	public int compare(D o1, D o2) {
 		return o1.compareInDimension(o2, currentDimension);
@@ -18,5 +25,15 @@ public class DimensionComparator<D extends Dimensionable<D>> implements Comparat
 
 	public void setCurrentDimension(int currentDimension) {
 		this.currentDimension = currentDimension;
+	}
+	
+	public DimensionComparator<D> nextDimension() {
+		this.currentDimension = (this.currentDimension + 1)%maxDimension;
+		return this;
+	}
+	
+	public DimensionComparator<D> previousDimension() {
+		this.currentDimension = (this.currentDimension + maxDimension - 1)%maxDimension;
+		return this;
 	}
 }
