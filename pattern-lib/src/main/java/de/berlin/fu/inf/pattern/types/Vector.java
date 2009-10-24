52197752c5f8891dc68b098486637762b9d6caba
wabu <waeber@inf.fu-berlin.de>
2009-10-24 19:00:21 +0200

    refactored Dimnsion Impl to Vector

--- pattern-lib/src/main/java/de/berlin/fu/inf/pattern/types/Vector.java
+++ pattern-lib/src/main/java/de/berlin/fu/inf/pattern/types/Vector.java
@@ -1,28 +1,29 @@
-package de.berlin.fu.inf.pattern.data.kdtree;
+package de.berlin.fu.inf.pattern.types;
 
 import java.util.Arrays;
 
-public class DimensionableImpl implements Dimensionable<DimensionableImpl> {
-	public static int DIMENSIONS = 3;
+import de.berlin.fu.inf.pattern.data.kdtree.Dimensionable;
+
+public class Vector implements Dimensionable<Vector> {
+	private final int[] vector;
+	private final int dimension;
 	
-	private int[] vector;
-	
-	public DimensionableImpl() {
-		this(new int[]{0,0,0});
+	public Vector(int dim) {
+		this(new int[dim]);
 	}
 	
-	public DimensionableImpl(int[] vector) {
-		this.setVector(vector);
+	public Vector(int[] vector) {
+		this.vector = vector.clone();
+		dimension = vector.length;
 	}
 	
 	public void setVector(int[] vector) {
-		if( vector != null && vector.length == DIMENSIONS) {
-			this.vector = vector;
-		}
+		assert vector.length == dimension;
+		System.arraycopy(vector, 0, this.vector, 0, dimension);
 	}
 	
 	
-	public int compareInDimension(DimensionableImpl dimObject, int dimension) {
+	public int compareInDimension(Vector dimObject, int dimension) {
 		if( !checkDimension(dimension) ) {
 			throw new IllegalArgumentException("dimension " + dimension + " is out of range");
 		}
@@ -34,18 +35,19 @@ public class DimensionableImpl implements Dimensionable<DimensionableImpl> {
 	/**
 	 * @return euklid distance between this and object
 	 */
-	public double getDistance(DimensionableImpl object) {
-		
+	public double getDistance(Vector object) {
 		double distance = 0.0d;
+		assert object.dimension == dimension;
 		
-		for(int i = 0; i < this.DIMENSIONS; i++) {
-			distance += Math.pow(getDistanceInDimension(object, i), 2);
+		for(int i = 0; i < dimension; i++) {
+			double d = getDistanceInDimension(object, i);
+			distance += d*d;
 		}
 		
 		return Math.sqrt(distance);
 	}
 
-	public double getDistanceInDimension(DimensionableImpl dimObject, int dimension) {
+	public double getDistanceInDimension(Vector dimObject, int dimension) {
 		
 		return Math.abs(getValueInDimension(dimension) - dimObject.getValueInDimension(dimension));
 	}
@@ -56,7 +58,7 @@ public class DimensionableImpl implements Dimensionable<DimensionableImpl> {
 	 * @return true if dimension is in range otherwise false
 	 */
 	private boolean checkDimension(int dimension) {
-		return dimension >= 0 && dimension < DIMENSIONS;
+		return dimension >= 0 && dimension < this.dimension;
 	}
 	
 	public int getValueInDimension(int dimension) {
@@ -70,8 +72,8 @@ public class DimensionableImpl implements Dimensionable<DimensionableImpl> {
 	
 	@Override
 	public boolean equals(Object obj) {
-		if(obj instanceof DimensionableImpl){
-			DimensionableImpl other = (DimensionableImpl) obj;
+		if(obj instanceof Vector){
+			Vector other = (Vector) obj;
 			return Arrays.equals(vector, other.vector);
 		}
 		return super.equals(obj);
@@ -88,9 +90,9 @@ public class DimensionableImpl implements Dimensionable<DimensionableImpl> {
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder("(");
-		for(int i = 0; i<DIMENSIONS; i++) {
+		for(int i = 0; i<dimension; i++) {
 			builder.append(vector[i]);
-			if(i<DIMENSIONS-1)
+			if(i<dimension-1)
 				builder.append(",");
 		}
 		builder.append(")");
