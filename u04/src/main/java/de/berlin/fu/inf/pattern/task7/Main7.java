95142a3dd2a1d0fd2003211fba72fba1dd787892
wabu <waeber@inf.fu-berlin.de>
2009-11-11 11:03:03 +0100

    added ne module

---
+++ u04/src/main/java/de/berlin/fu/inf/pattern/task7/Main7.java
@@ -0,0 +1,12 @@
+package de.berlin.fu.inf.pattern.task7;
+
+import org.apache.log4j.Logger;
+
+
+public class Main7 {
+	public static void main(String[] args) {
+		Logger logger = Logger.getRootLogger();
+		
+		logger.error("not implemented");
+	}
+}
