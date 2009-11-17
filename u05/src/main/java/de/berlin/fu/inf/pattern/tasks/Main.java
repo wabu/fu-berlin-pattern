f7bd9d3f5a6db0014ba98b36c547a8ea92e55072
wabu <waeber@inf.fu-berlin.de>
2009-11-17 18:48:15 +0100

    added main for jar file

---
+++ u05/src/main/java/de/berlin/fu/inf/pattern/tasks/Main.java
@@ -0,0 +1,9 @@
+package de.berlin.fu.inf.pattern.tasks;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Main10.main(args);
+	}
+}
