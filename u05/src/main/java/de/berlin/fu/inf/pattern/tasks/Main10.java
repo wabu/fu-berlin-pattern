830dee33378228d8c88386a3de4dc58abdedbd9b
wabu <waeber@inf.fu-berlin.de>
2009-11-17 18:42:16 +0100

    using common tester and other output

--- u05/src/main/java/de/berlin/fu/inf/pattern/tasks/Main10.java
+++ u05/src/main/java/de/berlin/fu/inf/pattern/tasks/Main10.java
@@ -1,19 +1,23 @@
 package de.berlin.fu.inf.pattern.tasks;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.log4j.Logger;
 
 import de.berlin.fu.inf.pattern.data.Entry;
 import de.berlin.fu.inf.pattern.iface.Classifier;
+import de.berlin.fu.inf.pattern.iface.DiscriminatingClassifier;
+import de.berlin.fu.inf.pattern.iface.SupervisedClassifier;
 import de.berlin.fu.inf.pattern.impl.fisher.FisherLinearDiscriminant;
 import de.berlin.fu.inf.pattern.impl.kdtree.KDClassificator;
 import de.berlin.fu.inf.pattern.util.data.DistributionGenerator;
 import de.berlin.fu.inf.pattern.util.data.DoubleVector;
 import de.berlin.fu.inf.pattern.util.gen.Generator;
 import de.berlin.fu.inf.pattern.util.gen.MultiNormalGenerator;
+import de.berlin.fu.inf.pattern.util.test.IntClassifierTest;
 import java.io.PrintStream;
 
 /**
@@ -27,12 +31,16 @@ import java.io.PrintStream;
 public class Main10 {
 
     private Logger logger = Logger.getLogger(Main10.class);
-    // begin at N 10 to avoid sigularities
-    private final int[] N = {10, 15, 20, 30, 40, 50, 100, 250, 500, 1000, 2000, 3000, 5000, 7500, 10000};
+
+    private final int[] N = {1,5,10, 15, 20, 30, 50, 100, 250, 500, 1000, 2000, 3000, 5000, 7500, 10000};
+
     private final DistributionGenerator gen = new DistributionGenerator();
+
     private final int maxDimension = 10;
-    private final int runs = 50;
+    // we use more runs to avoid high error for "bad" or "good" distingushable distributions
+    private final int runs = 7;
     private final int tests = 500;
+
     private String knnFile = "knnData.gp";
     private String fisherFile = "fisherData.gp";
     private PrintStream knnOutput = null;
@@ -44,7 +52,7 @@ public class Main10 {
 
         for (int dimension = 2; dimension <= maxDimension; dimension++) {
             for (int number : N) {
-                if (number >= dimension) { // avoid fisher sigularity
+                if (number >= maxDimension) { // avoid fisher sigularity
                     this.run(dimension, number);
                 }
             }
@@ -60,7 +68,7 @@ public class Main10 {
      * @param dim
      * @param elements
      */
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"unchecked"})
     private void run(int dim, int elements) {
         double classificationRateKNN = 0.0d;
         double classificationRateFisher = 0.0d;
@@ -70,19 +78,18 @@ public class Main10 {
         for (int run = 1; run < this.runs; run++) {
             logger.debug("run: " + run);
             // generate random distribution
-            Generator<DoubleVector> gen1 = new MultiNormalGenerator(dim);
-            Generator<DoubleVector> gen2 = new MultiNormalGenerator(dim);
+            Generator<DoubleVector> gen1 = new MultiNormalGenerator(dim, 2d);
+            Generator<DoubleVector> gen2 = new MultiNormalGenerator(dim, 2d);
 
-            Classifier<DoubleVector, Integer> knn =
-                    trainKNN(elements, gen1, gen2);
+            Classifier<DoubleVector, Integer> knn = trainKNN(elements, gen1, gen2);
             rate = runTest(tests, knn, gen1, gen2);
             logger.debug("KNN classified " + rate);
 
             classificationRateKNN += rate;
 
-            Classifier<DoubleVector, Integer> fisher =
-                    trainFisher(elements, dim, gen1, gen2);
+            Classifier<DoubleVector, Integer> fisher = trainFisher(elements, dim, gen1, gen2);
             rate = runTest(tests, fisher, gen1, gen2);
+            logger.debug("Fisher classified " + rate);
             classificationRateFisher += rate;
         } // end for(run)
         logger.info("======= KNN-rate = " + classificationRateKNN / runs
@@ -98,41 +105,35 @@ public class Main10 {
 
     private Classifier<DoubleVector, Integer> trainKNN(int size, Generator<DoubleVector> gen1, Generator<DoubleVector> gen2) {
         /// init KD-Classifier
-        KDClassificator<DoubleVector, Integer> kdClassifier =
+        SupervisedClassifier<DoubleVector, Integer> kdClassifier =
                 new KDClassificator<DoubleVector, Integer>();
+        logger.debug("trainging "+kdClassifier+" with "+size+" datapoints");
 
         // We need to transform our data to entry-sets
         Collection<Entry<DoubleVector, Integer>> classes =
-                new ArrayList<Entry<DoubleVector, Integer>>(2 * size);
-        classes.addAll(gen1.generateEntries(0, size));
-        classes.addAll(gen2.generateEntries(1, size));
+            Lists.newArrayList( Iterables.concat(
+               gen1.getEntryGenerator(0, size),
+               gen2.getEntryGenerator(1, size))
+            );
         // now train
         kdClassifier.train(classes);
         return kdClassifier;
     }
 
     private Classifier<DoubleVector, Integer> trainFisher(int size, int dim, Generator<DoubleVector> gen1, Generator<DoubleVector> gen2) {
-        FisherLinearDiscriminant<DoubleVector> fisher =
+        DiscriminatingClassifier<DoubleVector> fisher =
                 new FisherLinearDiscriminant<DoubleVector>(dim);
-        fisher.train(gen1.generate(size), gen2.generate(size));
+        logger.debug("trainging "+fisher+" with "+size+" datapoints");
+        fisher.train(
+                Lists.newArrayList(gen1.getGenerator(size)),
+                Lists.newArrayList(gen2.getGenerator(size)));
         return fisher;
     }
 
     public double runTest(int num, Classifier<DoubleVector, Integer> c, Generator<DoubleVector>... gens) {
-        int correct = 0;
-        double total = num * gens.length;
-
-        logger.debug("running " + num + " tests");
-        for (int i = 0; i < num; i++) {
-            int k = 0;
-            for (Generator<DoubleVector> g : gens) {
-                if (k == c.classify(g.generate())) {
-                    correct++;
-                }
-                k++;
-            }
-        }
-        return correct / total;
+        logger.debug("running "+num+" tests on "+c);
+        IntClassifierTest<DoubleVector> test = new IntClassifierTest<DoubleVector>(c);
+        return test.runTest(num, gens);
     }
 
     public static void main(String[] argv) throws FileNotFoundException {
