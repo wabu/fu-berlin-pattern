29f3df5c1bdc81d7c6a8478c2c7996d9e0920a15
wabu <waeber@inf.fu-berlin.de>
2009-11-26 16:54:15 +0100

    implemented Sigmoid

--- pattern-lib/src/main/java/de/berlin/fu/inf/pattern/util/fun/Sigmoid.java
+++ pattern-lib/src/main/java/de/berlin/fu/inf/pattern/util/fun/Sigmoid.java
@@ -5,10 +5,23 @@
 
 package de.berlin.fu.inf.pattern.util.fun;
 
+import org.jscience.mathematics.number.Float64;
+
 /**
  *
  * @author wabu
  */
-public class Sigmoid {
+public class Sigmoid implements Derivable<Float64, Float64>{
+    public Function<Float64, Float64> derive() {
+        return new Function<Float64, Float64>() {
+            public Float64 apply(Float64 x) {
+                Float64 o = Sigmoid.this.apply(x);
+                return o.times(Float64.ONE.minus(o));
+            }
+        };
+    }
 
+    public Float64 apply(Float64 x) {
+        return Float64.valueOf(1.0d/(1d+Math.exp(-x.doubleValue())));
+    }
 }
