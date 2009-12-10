f5de6d141b4daf95193488ab54e182955f1b389b
wabu <waeber@inf.fu-berlin.de>
2009-12-10 13:44:36 +0100

    new module for next task
    generated with local own archetype
    
    to install the archetype do:
    $ cd u-archetype-template
    $ mvn archetype:create-from-project
    $ cd target/generated-sources/archetype/
    $ mvn install
    
    now you can create a new project with
    $ mvn archetype:generate -DarchetypeCatalog=local

---
+++ u08/src/test/java/de/berlin/fu/inf/pattern/impl/perzeptron/TaskXTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.berlin.fu.inf.pattern.impl.perzeptron;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author wabu
+ */
+public class TaskXTest {
+    private final Logger logger = Logger.getLogger(TaskXTest.class);
+
+    @Test
+    public void runTest() {
+        logger.warn("test not implemented");
+        assertTrue("default test", true);
+     }
+}
