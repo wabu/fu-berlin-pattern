73bd474fc7ff97f54debcf7ceb2beba2b5d9110b
wabu <waeber@inf.fu-berlin.de>
2009-12-14 14:17:54 +0100

    improved archetype

--- u-archetype-template/src/main/java/de/berlin/fu/inf/pattern/tasks/Main.java
+++ u-archetype-template/src/main/java/de/berlin/fu/inf/pattern/tasks/Main.java
@@ -5,26 +5,33 @@
 
 package de.berlin.fu.inf.pattern.tasks;
 
+import de.berlin.fu.inf.pattern.util.Threads;
 import org.apache.log4j.Logger;
 
 /**
  *
  * @author wabu
  */
-public class Main {
-    private final static Logger logger = Logger.getLogger(Main.class);
+public final class Main {
+    private Main() {}
+
+    private final static Logger LOG = Logger.getLogger(Main.class);
     private static Runnable[] tasks = {
         new TaskX(),
     };
 
     public static void main(String args[]) throws InterruptedException {
-        for (Runnable t : tasks) {
-            logger.info("running "+t.getClass().getSimpleName());
-            try {
-                t.run();
-            } catch(Exception e) {
-                logger.error("execution of "+t.getClass()+" failed", e);
+        try {
+            for (Runnable t : tasks) {
+                LOG.info("running "+t.getClass().getSimpleName());
+                try {
+                    t.run();
+                } catch(Exception e) {
+                    LOG.error("execution of "+t.getClass()+" failed", e);
+                }
             }
+        } finally {
+            Threads.shutdown();
         }
     }
 }
